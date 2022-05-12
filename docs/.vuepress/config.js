2fc597868041b308742a207f942cb63a66b8e82d
tanrongfei <731429927@qq.com>
2022-05-12 10:36:29 +0800

    静态页面

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,7 +5,7 @@ module.exports = {
   head: [
     // ['link', { rel: 'icon', href: '/favicon.ico' }] // 需要被注入到当前页面的 HTML <head> 中的标签
     // <meta name="referrer" content="never"></meta>
-    ['meta', {name: 'referrer', content: 'never'}]
+    ["meta", { name: "referrer", content: "never" }],
   ],
   // host port在本地运行就不配置了
   themeConfig: {
@@ -44,9 +44,45 @@ module.exports = {
           { text: "网络协议", link: "/blog/web/network" },
           { text: "移动端", link: "/blog/web/mobile" },
           { text: "前端性能优化", link: "/blog/web/optimize" },
+          {
+            text: "浏览器工作原理与实践",
+            items: [
+              {
+                text: "01.Chrome架构：仅仅打开了1个页面，为什么有4个进程？",
+                link: "/blog/web/browser/01",
+              },
+              {
+                text: "02.TCP协议：如何保证页面文件能被完整送达浏览器？",
+                link: "/blog/web/browser/02",
+              },
+              {
+                text: "03.HTTP 请求流程：为什么很多站点第二次打开速度会很快？",
+                link: "/blog/web/browser/03",
+              },
+              {
+                text: "04.导航流程：从输入URL到页面展示，这中间发生了什么？",
+                link: "/blog/web/browser/04",
+              },
+            ],
+          },
         ],
       },
       { text: "linux", link: "/blog/linux/" },
+      {
+        text: "案例库",
+        items: [
+          { text: "文章收集", link: "/blog/case/article-collects" },
+          { text: "前端开发干货", link: "/blog/case/dry-goods" },
+        ],
+      },
+      {
+        text: "文档教程",
+        items: [
+          { text: "慕课教程", link: "https://www.imooc.com/wiki/" },
+          { text: "菜鸟教程", link: "https://www.runoob.com/" },
+          { text: "印记中文", link: "/blog/tutorial/03.印记中文" },
+        ],
+      },
     ],
     // 禁用导航栏
     // navbar: false,
